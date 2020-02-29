f801b448e6355373b454fd811777985a405016aa
edmondzxz <2300643457@qq.com>
2020-03-01 01:39:23 +0800

    delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "# discreteMathematic" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/edmondzxz/discreteMathematic.git
-git push -u origin master
